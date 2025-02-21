6039a6112e1d88fde44f77aad3328e80f2605cb5
Huzaifa Ishaque <huzaifaishaqueofficial@gmail.com>
2025-02-22 03:01:30 +0500

    Final Touch On Music Player

--- 9- Music Player App/script.js
+++ 9- Music Player App/script.js
@@ -2,7 +2,35 @@ let progress = document.getElementById('progress')
 let song = document.getElementById('song')
 let ctrlIcon = document.getElementById('ctrlIcon')
 
-song.onloadedmetadata = () =>{
-    progress.max = progress.duration;
-    progress.value = progress.currentTime;
+song.onloadedmetadata = () => {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+
+
+const playPause = () => {
+
+    if (ctrlIcon.classList.contains('bx-pause')) {
+        song.pause();
+        ctrlIcon.classList.remove('bx-pause')
+        ctrlIcon.classList.add('bx-play')
+    } else {
+        song.play();
+        ctrlIcon.classList.add('bx-pause')
+        ctrlIcon.classList.remove('bx-play')
+    }
+}
+
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = () => {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add('bx-pause')
+    ctrlIcon.classList.remove('bx-play')
 }
