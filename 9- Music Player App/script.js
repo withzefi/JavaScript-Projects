0188f0946b1847cbb9df19bd723e500376ee3022
Huzaifa Ishaque <huzaifaishaqueofficial@gmail.com>
2025-02-22 03:03:13 +0500

    Music Player App

--- 9- Music Player App/script.js
+++ 9- Music Player App/script.js
@@ -2,6 +2,8 @@ let progress = document.getElementById('progress')
 let song = document.getElementById('song')
 let ctrlIcon = document.getElementById('ctrlIcon')
 
+
+
 song.onloadedmetadata = () => {
     progress.max = song.duration;
     progress.value = song.currentTime;
@@ -22,11 +24,15 @@ const playPause = () => {
     }
 }
 
+
+
 if (song.play()) {
     setInterval(() => {
         progress.value = song.currentTime;
     }, 500);
 }
+
+
 
 progress.onchange = () => {
     song.play();
